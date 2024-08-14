74041fc86628dee59edbff84d04d15c1ed3eced0
phamdongchinh683 <phamdongchinh123789@gmail.com>
2024-08-14 23:35:36 +0700

    single linked list

---
+++ Linked_List_13_8_2024/Single_linked_list/main.java
@@ -0,0 +1,15 @@
+package Linked_List_13_8_2024.Single_linked_list;
+
+public class main {
+    public static void main(String[] args) {
+
+        SinglyLinkedList newList = new SinglyLinkedList();
+
+        newList.insert(12);
+        newList.insert(30);
+        newList.insert(50);
+        newList.insert(70);
+        newList.insert(100);
+        newList.displayList();
+    }
+}
